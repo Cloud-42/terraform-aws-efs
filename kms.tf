72d48db523e54094a8ba25a4ac854881f82e5bbf
Cloud42 <hello@cloud42.io>
2020-12-17 11:46:26 +0000

    Encryption is optional

--- kms.tf
+++ kms.tf
@@ -1,16 +1,15 @@
 # -------------------------------------------------------
 # KMS Key for EFS
 # -------------------------------------------------------
-resource "aws_kms_key" "efskey" {
+resource "aws_kms_key" "this" {
+  count                   = var.efs_encrypted == true ? 1 : 0
   description             = "This key is used to encrypt EFS in ${var.environment}"
   deletion_window_in_days = 30
 }
 
-#
 # Alias
-#
-
-resource "aws_kms_alias" "efs" {
+resource "aws_kms_alias" "this" {
+  count                   = var.efs_encrypted == true ? 1 : 0
   name          = "alias/efs-${var.environment}-${var.build_ref}"
   target_key_id = aws_kms_key.efskey.key_id
 }
