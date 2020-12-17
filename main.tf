96d7e04a63ed809960f90bc3d82a84391b84dd69
Cloud42 <hello@cloud42.io>
2020-12-17 12:37:51 +0000

    correct names

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ resource "aws_efs_file_system" "this" {
 # --------------------------
 resource "aws_efs_mount_target" "subnet_a" {
   depends_on      = [aws_efs_file_system.this]
-  count           = var.private_subnet_a != "" ? 1 : 0
+  count           = var.subnet_a != "" ? 1 : 0
   file_system_id  = aws_efs_file_system.this.id
-  security_groups = var.security_groups_subnet_a
+  security_groups = var.security_group_subnet_a
   subnet_id       = var.subnet_a
 }
 
 resource "aws_efs_mount_target" "subnet_b" {
   depends_on      = [aws_efs_file_system.this]
-  count           = var.private_subnet_b != "" ? 1 : 0
+  count           = var.subnet_b != "" ? 1 : 0
   file_system_id  = aws_efs_file_system.this.id
-  security_groups = var.security_groups_subnet_a
+  security_groups = var.security_group_subnet_a
   subnet_id       = var.subnet_b
 }
