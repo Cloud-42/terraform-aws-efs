9fe541d7050edc9fdc2f349b8460241768c3ccd4
Cloud-42 <hello@cloud42.io>
2021-12-29 12:11:08 +0000

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,11 @@ output "efs_dns_name" {
   description = "DNS name of the EFS share"
   value       = aws_efs_file_system.this.dns_name
 }
-
+output "efs_arn" {
+  description = "ARN of the EFS share"
+  value       = aws_efs_file_system.this.arn
+}
+output "efs_number_of_mount_targets" {
+  description = "number of mount targets"
+  value       = aws_efs_file_system.this.number_of_mount_targets
+}
